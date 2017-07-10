9979905c62a89d7e28efebfee301ddbe951948aa
jianghaitian <jianghaitian@gomeplus.com>
2017-07-10 18:29:42 +0800

    HTEffiency

--- HTUtilties/HTUtilties/HTEfficiency.h
+++ HTUtilties/HTUtilties/HTEfficiency.h
@@ -10,7 +10,9 @@
 
 void HTDispatchAsyncMain(dispatch_block_t block);
 void HTDispatchAsyncGlobal(dispatch_block_t block);
+void HTDispatchSyncSerial(dispatch_block_t block);
 void HTDispatchAfter(float sec, dispatch_block_t block);
+void HTDispatchGlobalThenMain(dispatch_block_t globalBlock, dispatch_block_t mainBlock);
 
 @interface HTEfficiency : NSObject
 
@@ -19,8 +21,8 @@ void HTDispatchAfter(float sec, dispatch_block_t block);
 
 @interface NSArray<__covariant ObjectType> (HTEfficiency)
 
-- (NSMutableArray<ObjectType> *)ht_filterWithBlock:(BOOL (^)(ObjectType obj))block;
+- (NSMutableArray<ObjectType> *)ht_filterWithBlock:(BOOL (^)(ObjectType ele))block;
 
-- (NSMutableArray *)ht_mapWithBlock:(id (^)(ObjectType obj))block;
+- (NSMutableArray *)ht_mapWithBlock:(id (^)(ObjectType ele))block;
 
 @end
--- HTUtilties/HTUtilties/HTEfficiency.m
+++ HTUtilties/HTUtilties/HTEfficiency.m
@@ -18,9 +18,27 @@ inline void HTDispatchAsyncGlobal(dispatch_block_t block)
 	dispatch_async(dispatch_get_global_queue(0, 0), block);
 }
 
+void HTDispatchSyncSerial(dispatch_block_t block)
+{
+	static dispatch_once_t onceToken;
+	static dispatch_queue_t serialQueue;
+	dispatch_once(&onceToken, ^{
+		serialQueue = dispatch_queue_create("HTEfficiency.serialQueue", DISPATCH_QUEUE_SERIAL);
+	});
+	dispatch_async(serialQueue, block);
+}
+
 inline void HTDispatchAfter(float sec, dispatch_block_t block)
 {
 	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(sec * NSEC_PER_SEC)), dispatch_get_main_queue(), block);
+}
+
+inline void HTDispatchGlobalThenMain(dispatch_block_t globalBlock, dispatch_block_t mainBlock)
+{
+	HTDispatchAsyncGlobal(^{
+		globalBlock();
+		HTDispatchAsyncMain(mainBlock);
+	});
 }
 
 @implementation NSArray (HTEfficiency)
@@ -28,9 +46,9 @@ inline void HTDispatchAfter(float sec, dispatch_block_t block)
 - (NSMutableArray *)ht_filterWithBlock:(BOOL (^)(id))block
 {
 	NSMutableArray *newArray = [NSMutableArray new];
-	for (id obj in self) {
-		if (block(obj)) {
-			[newArray addObject:obj];
+	for (id ele in self) {
+		if (block(ele)) {
+			[newArray addObject:ele];
 		}
 	}
 	return newArray;
@@ -39,8 +57,8 @@ inline void HTDispatchAfter(float sec, dispatch_block_t block)
 - (NSMutableArray *)ht_mapWithBlock:(id (^)(id))block
 {
 	NSMutableArray *newArray = [NSMutableArray new];
-	for (id obj in self) {
-		id mappedObj = block(obj);
+	for (id ele in self) {
+		id mappedObj = block(ele);
 		if (mappedObj) {
 			[newArray addObject:mappedObj];
 		}
